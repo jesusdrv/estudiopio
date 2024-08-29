e7c09726f98fdc94052eed7a320d930ae855154f
jesusdrv <artorias1989@gmail.com>
2024-08-28 21:18:11 -0500

    primer comentario

---
+++ fundamentos.py
@@ -0,0 +1,31 @@
+# nombre = "Bienvenidos"
+# num = 45
+# decimal = 2.6
+# pregunta = True
+
+# print (nombre)
+# print (num)
+# print (decimal)
+# print (pregunta)
+
+# edad = 20 
+# print (edad)
+
+# nombre = input("Ingrese su nombre")
+# edad = input("Ingrese edad")
+# print("usted es",nombre, "y tiene",edad, "años")
+
+print ("Hola" == "Hola")
+print ("Hola" != "Python")
+print ("Hola" < "Python")
+
+nombre, edad, telefono = "Jesus" , 35, 3205893786
+
+print("Soy {} tengo {} y mi telefono es {}".format(nombre,edad,telefono))
+print("Soy " + nombre + ", tengo " + str(edad) + " años y mi teléfono es " + str(telefono))
+
+
+
+
+
+
